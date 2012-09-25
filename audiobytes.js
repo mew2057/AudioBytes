b727ade6c74f2760c51dbc63e875803765778409
John Dunham <jfdbear1@gmail.com>
2012-09-25 13:51:43 -0400

    Playing around with some HTML5 techniques, but it seems that drag and drop file loading may be somewhat harder than I initially had assumed. Bouncing off a server would be easier I think, but I don't have the resources and it spits in the initial concept of the game.

--- audiobytes.js
+++ audiobytes.js
@@ -15,10 +15,54 @@ AudioBytes.init = function()
     $("#AudioBytesCanvas").css("outline","0");    
     $("#AudioBytesCanvas").focus();
     
+    //Uses FileReader, Audio API,Session Storage
+    
     // Sets up the canvas and drawing context details for the game.
     audioBytesGame.canvas = document.getElementById("AudioBytesCanvas");
     audioBytesGame.canvas.width = 500;
     audioBytesGame.canvas.height = 500;
+    
 
     audioBytesGame.context = audioBytesGame.canvas.getContext("2d");      
+    
+    audioBytesGame.context.fillStyle="white";
+    audioBytesGame.context.fillRect(0,0,500,500);
+    $("#AudioBytesCanvas").bind("drop",audioBytesGame.drop);
+    $("#AudioBytesCanvas").bind("dragenter",audioBytesGame.dragEnter);
+    $("#AudioBytesCanvas").bind("dragover", audioBytesGame.dragOver);
+};
+
+AudioBytes.prototype.dragEnter = function(e)
+{
+    e.stopPropagation();
+    e.preventDefault();
+};
+
+AudioBytes.prototype.dragOver = function(e)
+{   
+    e.stopPropagation();
+    e.preventDefault();
+};
+
+AudioBytes.prototype.drop = function(e)
+{
+    e.stopPropagation();
+    e.preventDefault();
+    if(e.originalEvent.dataTransfer.files[0].type != "audio/mp3")
+    {
+        alert("Invalid file type, only mp3 files are currently supported!");   
+        return false;
+    }
+    var reader = new FileReader();
+
+    reader.onload = function(event)
+    {
+        //This will be harder then I thought....
+        /*localStorage.setItem("song",event.target.result);
+        
+        $('<audio id="audiocanvas" tabIndex="0" src=blah>HTML5 not supported in your browser</audio>').appendTo("body");
+    alert(localStorage.getItem('song'));
+        $('#audiocanvas').play();*/
+    };
+    reader.readAsDataURL(e.originalEvent.dataTransfer.files[0]);
 };
