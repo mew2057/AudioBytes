3f540102ad8c7db934faf886dc367273b0056716
John Dunham <jfdbear1@gmail.com>
2012-09-29 23:49:21 -0400

    Switched over to the request animation frame for my game loop (this is apparently a best practice). Instead of drawing the whole waveform simultaneously I now output it timeline style. I'm still working on optimizations for this model(since I;ve noticed occasional jitters), but for the most part it is performing nicely. I still have no idea how the worlds will be generated, but thought experiments are presently being run.

--- audiobytes.js
+++ audiobytes.js
@@ -2,6 +2,18 @@
  * Of Interest:
  *  http://airtightinteractive.com/demos/js/reactive/
  */
+ 
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       || 
+          window.webkitRequestAnimationFrame || 
+          window.mozRequestAnimationFrame    || 
+          window.oRequestAnimationFrame      || 
+          window.msRequestAnimationFrame     || 
+          function(/* function */ callback, /* DOMElement */ element){
+            window.setTimeout(callback, 20);
+          };
+})();
+ 
 function AudioBytes()
 {
     this.canvas = null;
@@ -11,13 +23,16 @@ function AudioBytes()
     this.audioSource = null;
     this.audioAnalyzer = null;
     this.audioScratch = null;
+    this.audioScratchIndex = 0;
+    this.audioDeque = null;
 }
 
 AudioBytes._Game = null;
+AudioBytes.startY = 0;
 
 AudioBytes.prototype.draw = function()
 {
-    this.context.fillRect(0,0,1024,500);
+    this.context.fillRect(0,0,this.canvas.width,this.canvas.height);
 
     this.drawWave();
 
@@ -25,34 +40,41 @@ AudioBytes.prototype.draw = function()
 
 AudioBytes.prototype.drawWave = function()
 {
-    var startY = this.canvas.height/2;
     this.context.beginPath();
 
+    this.context.moveTo(0,AudioBytes.startY + this.audioDeque[0] - 128);
 
-    this.context.moveTo(0,startY + this.audioScratch[0] - 128);
-
-    for (var cell  = 1; cell <  this.audioScratch.length; cell++)
+    for (var cell  = 1, renderPos = 2; cell <  this.audioDeque.length; cell++, renderPos +=4)
     {
-        this.context.lineTo(cell,startY + this.audioScratch[cell] - 128);
+        this.context.lineTo(renderPos,AudioBytes.startY + this.audioDeque[cell] - 128);
     }
+    this.context.lineTo(renderPos,AudioBytes.startY);
     this.context.stroke();
 };
 
 AudioBytes.prototype.update = function()
 {
+    if(this.audioScratchIndex >= this.audioScratch.length || isNaN(this.audioScratch[0]))
+    {
+        this.audioAnalyzer.getByteTimeDomainData(this.audioScratch);
+        this.audioScratchIndex = 0;
+    }
+            this.audioAnalyzer.getByteTimeDomainData(this.audioScratch);
 
-   this.audioAnalyzer.getByteTimeDomainData(this.audioScratch);
+    this.audioDeque.shift();
+    this.audioDeque.push(this.audioScratch[this.audioScratchIndex]);
 };
 
-AudioBytes.prototype.gameLoop = function()
+//Philosophically only one gameloop.
+AudioBytes.gameLoop = function()
 {
     
-    this.update();
-    this.draw();
+    AudioBytes._Game.update();
 
-    setTimeout(function() {
-        AudioBytes._Game.gameLoop();
-    }, 16.6);
+    AudioBytes._Game.draw();
+    
+    window.requestAnimFrame(AudioBytes.gameLoop);
+
 };
 
 AudioBytes.init = function()
@@ -62,8 +84,19 @@ AudioBytes.init = function()
     try {
         AudioBytes._Game.audioContext = new window.webkitAudioContext();
         AudioBytes._Game.audioAnalyzer  = AudioBytes._Game.audioContext.createAnalyser();
+        AudioBytes._Game.audioDeque = [];
+                
+        for (var cell = 0; cell < AudioBytes._Game.audioAnalyzer.frequencyBinCount/4; 
+            cell ++ )
+        {
+            AudioBytes._Game.audioDeque.push(128);
+        }
+        
         AudioBytes._Game.audioScratch = new Uint8Array(
-                AudioBytes._Game.audioAnalyzer.frequencyBinCount);
+            1);
+                
+        AudioBytes._Game.audioAnalyzer.getByteTimeDomainData(
+            AudioBytes._Game.audioScratch);
 
     }
     catch(e) {
@@ -71,7 +104,8 @@ AudioBytes.init = function()
     }
     
     // Establishes the canvas with an id and a tabindex.
-    $('<canvas id="AudioBytesCanvas" tabIndex="0">HTML5 not supported in your browser</canvas>').appendTo('body');
+    $('<canvas id="AudioBytesCanvas" tabIndex="0">HTML5 not supported in your' +
+        'browser</canvas>').appendTo('body');
     
     //Removes the focus border.
     $("#AudioBytesCanvas").css("outline","0");    
@@ -85,20 +119,23 @@ AudioBytes.init = function()
     AudioBytes._Game.canvas = document.getElementById("AudioBytesCanvas");
     AudioBytes._Game.canvas.width = 1024;
     AudioBytes._Game.canvas.height = 500;
-    
+    AudioBytes.startY = AudioBytes._Game.canvas.height/2;
 
     AudioBytes._Game.context = AudioBytes._Game.canvas.getContext("2d");      
     
     AudioBytes._Game.context.fillStyle="white";
-    AudioBytes._Game.context.fillRect(0,0,1024,500);
-        AudioBytes._Game.context.strokeStyle="black";
+    AudioBytes._Game.context.fillRect(0,0,AudioBytes._Game.canvas.width,AudioBytes._Game.canvas.height);
+    
+    AudioBytes._Game.context.strokeStyle="blue";
+    AudioBytes._Game.context.lineWidth = 2.0;
         
 
     
     $("#AudioBytesCanvas").bind("drop",AudioBytes._Game.drop);
     $("#AudioBytesCanvas").bind("dragenter",AudioBytes._Game.dragEnter);
     $("#AudioBytesCanvas").bind("dragover", AudioBytes._Game.dragOver);
-    AudioBytes._Game.gameLoop();
+    
+    AudioBytes.gameLoop();
 };
 
 AudioBytes.prototype.dragEnter = function(e)
@@ -148,8 +185,11 @@ AudioBytes.prototype.startSong = function(buffer)
     this.audioSource = this.audioContext.createBufferSource();
     this.audioSource.buffer = buffer;
     this.audioSource.connect(this.audioContext.destination);
-    this.audioSource.connect(this.audioAnalyzer);
+    this.audioSource.connect(this.audioAnalyzer);    
+    
     this.audioSource.noteOn(0);
+    this.audioScratchIndex = this.audioScratch.length;
+
 };
 
 AudioBytes.prototype.loadError = function()
