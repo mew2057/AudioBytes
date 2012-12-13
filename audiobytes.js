2ab471787d1d94d94c568a49537696d0592b53ed
John Dunham <jfdbear1@gmail.com>
2012-12-13 14:09:50 -0500

    Added comments, did some clean up work and made the time domain data into an equalizer.

--- audiobytes.js
+++ audiobytes.js
@@ -1,7 +1,16 @@
-/**
- * Of Interest:
- *  http://airtightinteractive.com/demos/js/reactive/
- */
+/* ------------
+   audiobytes.js
+   
+   Requires controller.js, sprite.js
+   
+   Contains the main gameplay code for audiobytes: loading the audio with the 
+   webkit audio context, drawing the waveform data and hosting the game loop.   
+   
+   Of Interest:
+     http://airtightinteractive.com/demos/js/reactive/
+   This example is what made me realize at least the audio processing in browser 
+     was possible and resulted in me pursuing this project.
+   ------------ */
  
 window.requestAnimFrame = (function(){
   return  window.requestAnimationFrame       || 
@@ -13,125 +22,193 @@ window.requestAnimFrame = (function(){
             window.setTimeout(callback, 100);
           };
 })();
- 
+
+/**
+ * The class definition of the Audio Bytes Game state.
+ */
 function AudioBytes()
 {
-    this.canvas = null;
-    this.context = null;
-    this.audioContext = null;
-    this.newAudioQueued = false;
-    this.audioSource = null;
-    this.audioAnalyzer = null;
-    this.audioScratch = null;
-    this.audioScratchIndex = 0;
-    this.audioDeque = null;
-    this.isLoading = false;
-    this.drawFunct = this.drawWave;
-    this.actor = null;
-    this.controller = null;
+    this.canvas = null;                 // The HTML5 canvas.
+    this.context = null;                // The drawing context for the game.
+    this.audioContext = null;           // The audio context for the game.
+    this.audioSource = null;            // The source variable that is associated with the context (actaully plays the music).
+    this.audioAnalyzer = null;          // Performs the FFT on the audio data (a webkit audio api variable).
+    this.audioScratch = null;           // A scratch buffer for audio analyzer data.
+    this.domainScratch = null;          // A scratch buffer for domain data that is used in drawing the bars.
+    this.drawFunct = this.drawWave;     // A pointer to the currently active drawing function.
+    this.actor = null;                  // The playable character in the game.
+    this.controller = null;             // The player controller.
+    
+    this.audioDeque = null;             // A deque for audio data (currently unused)
+    this.audioDeque2 = null;            // A deque for audio data (used for frequency data)
+
 }
 
+// The psuedo singleton game object.
 AudioBytes._Game = null;
+
+// The horizon of the game.
 AudioBytes.startY = 0;
 
+/**
+ * The draw function for the game loop.
+ */
 AudioBytes.prototype.draw = function()
 {
+    // Clears out the canvas.
     this.context.fillRect(0,0,this.canvas.width,this.canvas.height);
-
-    this.context.save();
-
-    this.context.globalCompositeOperation = 'darker';
-
-    this.context.fillStyle = '#09f000';
-    this.context.fillRect(this.canvas.width - 50,0,50, this.canvas.height);
     
+    // Invoke the variable draw.
     this.drawFunct();
+    
+    // Only draw if the actor is present
     if(this.actor)
         this.actor.draw();
+};
+
+/**
+ * Draws the output of the FFT. Eventually this will be replaced by a procedural generation function.
+ */
+AudioBytes.prototype.drawWave = function()
+{
+    this.context.save();
+    
+    // Draw the horizon.
+    
+    this.context.beginPath();
+    this.context.strokeStyle = "black";
+
+    this.context.moveTo(0,AudioBytes.startY);
+    this.context.lineTo(1024, AudioBytes.startY);
+    
+    this.context.stroke();
+    
+    // Draw the time domain.
+
+    this.context.fillStyle = "black";
+    
+    this.context.moveTo(0,this.canvas.height - this.domainScratch[0]);
+
+    for (var cell  = 1, renderPos = 2; cell <  this.domainScratch.length; cell++, renderPos += 4)
+    {
+        this.context.fillRect(renderPos,this.canvas.height,2 ,-(this.domainScratch[cell]));
+    }
+    
+    // Draw the frequency data
+    
+    this.context.beginPath();
+    
+    this.context.strokeStyle = "blue";
+    this.context.moveTo(0,AudioBytes.startY - this.audioDeque2[0]);
+
+    for (var cell  = 1, renderPos = 2; cell <  this.audioDeque.length; cell++, renderPos += 4)
+    {
+        this.context.lineTo(renderPos + 4,AudioBytes.startY - this.audioDeque2[cell]);
+    }
+    
+    this.context.lineTo(renderPos,AudioBytes.startY);
+    this.context.stroke();
+    
     this.context.restore();
 };
 
-AudioBytes.prototype.drawWave = function()
+/**
+ * A convenience function for a hypothetical procedural generation technique. 
+ */
+AudioBytes.sumArray = function(toSum)
 {
+    var ret = 0;
     
-    this.context.beginPath();
-
-    this.context.moveTo(0,AudioBytes.startY + this.audioDeque[0] - 128);
-    
-    for (var cell  = 1, renderPos = 2; cell <  this.audioDeque.length; cell++, renderPos += 4)
-    {
-        this.context.moveTo(renderPos, AudioBytes.startY + this.audioDeque[cell] - 128);
-        this.context.lineTo(renderPos +4,AudioBytes.startY + this.audioDeque[cell] - 128);
-    }
-    this.context.lineTo(renderPos,AudioBytes.startY);
-    this.context.stroke();
+    for(var index = 0; index < toSum.length; index++)
+        ret +=toSum[index];
+        
+    return ret;
 };
 
+/**
+ * The update function to be called in the game loop.
+ */
 AudioBytes.prototype.update = function()
 {
-    /*if(this.audioScratchIndex >= this.audioScratch.length || isNaN(this.audioScratch[0]))
-    {
-        this.audioAnalyzer.getByteTimeDomainData(this.audioScratch);
-        this.audioScratchIndex = 0;
-    }*/
+    this.audioAnalyzer.getByteTimeDomainData(this.domainScratch);
+ 
+    this.audioAnalyzer.getByteFrequencyData(this.audioScratch);
+    this.audioDeque2.shift();
+    this.audioDeque2.push(256-this.audioScratch[0]);
     
-    this.audioAnalyzer.getByteTimeDomainData(this.audioScratch);
-
-    // Make this dynamic based on the current frequency?
-    this.audioDeque.shift();
-    this.audioDeque.push(this.audioScratch[0]);
-//    this.audioAnalyzer.getByteFrequencyData(this.audioScratch);
     if(this.actor)
         this.actor.update();
 };
 
-//Philosophically only one gameloop.
+/**
+ * The audio bytes game loop.
+ */
 AudioBytes.gameLoop = function()
 {    
     AudioBytes._Game.update();
-
     AudioBytes._Game.draw();
     
     window.requestAnimFrame(AudioBytes.gameLoop);
 
 };
 
-// does a super basic check (This is wicked inaccurate!).
+/**
+ * Performs a global collide check for an actor. Right now it only performs a 
+ * horizon check and clipping has NOT been fully tested.
+ * 
+ * @param actor The gameplay element that is being checked for a collision.
+ * 
+ * @return true if a world collision was found, false if none.
+ */
 AudioBytes.collides = function(actor)
 {
     if(actor.sprites.length > 0)
-        for(var index = actor.x; index < actor.x + actor.sprites[actor.currentSprite].w ; index++)
+    {
+        if(actor.y <= AudioBytes.startY && actor.y >=  AudioBytes.startY - actor.velocity[1])
         {
-            if(AudioBytes._Game.audioDeque[index] <= actor.y - AudioBytes.startY + 128 && 
-                AudioBytes._Game.audioDeque[index] >= actor.y - AudioBytes.startY + 128  - actor.velocity[1])
-            {
-                actor.y = AudioBytes._Game.audioDeque[index] + AudioBytes.startY - 128;
-                return true;
-            }
+            actor.y = AudioBytes.startY;
+            return true;
         }
+    }
             
     return false;
 };
 
+/**
+ * Initializes the game.
+ */
 AudioBytes.init = function()
 {
+    // Set the psuedo singleton game state
     AudioBytes._Game = new AudioBytes();
+    
+    // Establish the world collide callback.
     Actor.GAME_WORLD_COLLIDE_CALLBACK = AudioBytes.collides;
     
+    // Try to load the audio context, if a failure occurs the game is unplayable.
     try {
+        // Set up all the audio data structures.
         AudioBytes._Game.audioContext = new window.webkitAudioContext();
         AudioBytes._Game.audioAnalyzer  = AudioBytes._Game.audioContext.createAnalyser();
         AudioBytes._Game.audioDeque = [];
-                
-        for (var cell = 0; cell < AudioBytes._Game.audioAnalyzer.frequencyBinCount/4; 
-            cell ++ )
+        AudioBytes._Game.audioDeque2 = [];
+        AudioBytes._Game.audioAnalyzer.frequencyBinCount = 256;
+        
+        // Initialize the deques
+        for (var cell = 0; cell < 256; cell ++ )
         {
             AudioBytes._Game.audioDeque.push(128);
+            AudioBytes._Game.audioDeque2.push(0);
         }
         
+        // Set up the scratch  space for using the audio context analyzer.
         AudioBytes._Game.audioScratch = new Uint8Array(
             AudioBytes._Game.audioAnalyzer.frequencyBinCount);
-                
+        
+        AudioBytes._Game.domainScratch = new Uint8Array(
+            AudioBytes._Game.audioAnalyzer.frequencyBinCount);
+        
+        // Initialize the scratch.
         AudioBytes._Game.audioAnalyzer.getByteTimeDomainData(
             AudioBytes._Game.audioScratch);
 
@@ -153,13 +230,11 @@ AudioBytes.init = function()
     
     //Uses FileReader, Audio API,Session Storage
 
-    AudioBytes._Game.newAudioQueued = false;
-
     // Sets up the canvas and drawing context details for the game.
     AudioBytes._Game.canvas = document.getElementById("AudioBytesCanvas");
     AudioBytes._Game.canvas.width = 1020;
     AudioBytes._Game.canvas.height = 500;
-    AudioBytes.startY = AudioBytes._Game.canvas.height/2;
+    AudioBytes.startY = AudioBytes._Game.canvas.height/2 + AudioBytes._Game.canvas.height/8;
 
     AudioBytes._Game.context = AudioBytes._Game.canvas.getContext("2d");      
     
@@ -170,43 +245,59 @@ AudioBytes.init = function()
     AudioBytes._Game.context.lineWidth = 2.0;
         
 
+    // Initialize the actor that the user will control.
     AudioBytes._Game.actor = Player.initFromFile("spriteSheet.json",AudioBytes._Game.context);
     AudioBytes._Game.actor.x = 25;
     AudioBytes._Game.actor.y = 200;
     
+    // Set up the controller (mouse clicks)
     AudioBytes._Game.controller = new Controller();
     AudioBytes._Game.controller.init(AudioBytes._Game.actor);
     
+    // Set up the dragging functionality.
     window.addEventListener("drop",AudioBytes._Game.drop);
     window.addEventListener("dragenter",AudioBytes._Game.dragEnter);
     window.addEventListener("dragover", AudioBytes._Game.dragOver);
     
+    // Tell the user what to do.
     alert("Drag an mp3 onto the page.");
     AudioBytes.gameLoop();
 };
 
+/**
+ * Drag Enter event, at present just stops propagation.
+ */
 AudioBytes.prototype.dragEnter = function(e)
 {
     e.stopPropagation();
     e.preventDefault();
 };
 
+/**
+ * Drag Over event, at present just stops propagation.
+ */
 AudioBytes.prototype.dragOver = function(e)
 {   
     e.stopPropagation();
     e.preventDefault();
 };
 
+/**
+ * Drop event, this is where the magic happens.
+ * Loads the audio file.
+ */
 AudioBytes.prototype.drop = function(e)
 {
     e.stopPropagation();
     e.preventDefault();
 
+    // Verify the file is valid for the audio context.
     if(e.dataTransfer.files[0].type != "audio/mp3")
     {
         alert("Invalid file type, only mp3 files are currently supported!");   
         return false;
     }
+    
     var reader = new FileReader();
 
     reader.onload = function(event)
@@ -214,41 +305,56 @@ AudioBytes.prototype.drop = function(e)
         AudioBytes._Game.audioContext.decodeAudioData(event.target.result, function(buffer) {
             AudioBytes._Game.startSong(buffer);
         }, AudioBytes._Game.loadError);
-        
-        //Add an animation drawer here in the end game, to give the user feedback.
     };
     
+    // Set the animation for the load to the loading bar
     AudioBytes._Game.drawFunct = AudioBytes._Game.drawLoad;
-    console.log(e.dataTransfer.files[0]);
     reader.readAsArrayBuffer(e.dataTransfer.files[0]);
 };
 
+/**
+ * Kicks off the song after it has been loaded.
+ */
 AudioBytes.prototype.startSong = function(buffer)
 {
+    // Stops any audio that is currently playing.
     if(this.audioSource)
     {
         this.audioSource.noteOff(0);
     }
     
+    // create a new audio buffer for the song.
     this.audioSource = this.audioContext.createBufferSource();
     this.audioSource.buffer = buffer;
+    
+    // Connects the analyzer and the context destination to the audio context.
     this.audioSource.connect(this.audioContext.destination);
     this.audioSource.connect(this.audioAnalyzer);    
     
+    // Start the song and change the drawing function.
     this.audioSource.noteOn(0);
-    this.audioScratchIndex = this.audioScratch.length;
     AudioBytes._Game.drawFunct = AudioBytes._Game.drawWave;
-    };
+    
+    
+    // TODO find a better solution for this.
+    width = 0;
+};
 
+/**
+ * An error callback for load errors.
+ */
 AudioBytes.prototype.loadError = function()
 {
     alert("Your song has failed to decode, I am sorry");  
-    this.isLoading = false;
+    
+    // reset the draw function.
     AudioBytes._Game.drawFunct = AudioBytes._Game.drawWave;
-
 };
 
+// Width of the loading bar.
 var width = 0;
+
+// Draws the loading bar.
 AudioBytes.prototype.drawLoad = function()
 {
     this.context.save();
@@ -257,6 +363,7 @@ AudioBytes.prototype.drawLoad = function()
     this.context.fillStyle = "blue";
     this.context.fillRect(this.canvas.width/2 - width, this.canvas.height/2, width *2,50); 
     this.context.fill();
-    width = (width+1);
+    width++;
+    
     this.context.restore();
 };
--- controller.js
+++ controller.js
@@ -1,14 +1,30 @@
-
+/* ------------
+   controller.js
+   
+   Defines the controller functionality for audiobytes.
+   ------------ */
+   
+/**
+ * The class definition for the audiobytes controller.
+ */
 function Controller()
 {
+    // The player the controller is linked to.
     this.player = null;
 }
 
+/**
+ * The mouse click event which send a jump signal to the player.
+ */
 Controller.prototype.mouseClick = function()
 {
     this.player.queuedInput.push("jump");
 };
 
+/**
+ * Initializes the controller with the player and begins listing to button inputs.
+ * @param player The linkedplayer.
+ */
 Controller.prototype.init = function(player)
 {
     var self = this;
